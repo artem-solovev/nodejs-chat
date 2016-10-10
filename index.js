c873939d84abf3b4620c8a9fb0b185d3a62b1d06
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-10 15:34:33 +0300

    Added basic SOCKET.IO functionality

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var port = 8080;
+
 var socket = require( "socket.io" );
 var express = require( "express" );
 var app = express();
 var http = require( "http" ).Server( app );
+var io = require( "socket.io" )( http );
+
 
 
 
 app.get( "/", function( request, response ) {
     response.sendFile( __dirname + "/views/index.html" );
 } );
+
+io.on( "connection", function( socket ){
+    console.log( "User is logged in" );
+
+    socket.on( "disconnect", function() {
+        console.log( "User is logged out" );
+    } );
+});
 
 http.listen( port, function() {
     console.log( "Listening on : " + port );
