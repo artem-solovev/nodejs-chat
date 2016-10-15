4456dc0e5b86b4177f249af9fed9e9b4bd897ad5
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-15 15:49:45 +0300

    Added getName function

--- src/public/client.js
+++ src/public/client.js
@@ -1,38 +1,44 @@
 var socket = io();
 
-            var nickname = null;
+var nickname = null;
 
-            window.onload = function() {
-                openNav();
+window.onload = function() {
+    openNav();
 
+    nickname = getName();
 
-
-                while ( nickname == null ) {
-                    nickname = prompt( "What is your nickname?", "" );
-                }
-
-                socket.emit( "user nickname", nickname );
-                $( "#username" ).text( "Hello " + nickname );
-            };
+    socket.emit( "user nickname", nickname );
+    $( "#username" ).text( "Hello " + nickname );
+};
 
 
-            $( "form" ).submit( function() {
-                socket.emit( "chat message", $( "#messageForm" ).val() );
-                $( "#messageForm" ).val( "" );
+$( "form" ).submit( function() {
+    socket.emit( "chat message", $( "#messageForm" ).val() );
+    $( "#messageForm" ).val( "" );
 
-                return false;
-            } );
+    return false;
+} );
 
-            socket.on( "user nickname", function( nickname ) {
-                $( "#messages" ).append( $( "<li>" ).css( "font-weight", "700" ).text( nickname + " joined us" )  );
-            } );
+socket.on( "user nickname", function( nickname ) {
+    $( "#messages" ).append( $( "<li>" ).css( "font-weight", "700" ).text( nickname + " joined us" )  );
+} );
 
-            socket.on( "chat message", function( msg ) {
-                $( "#messages" ).append( $( "<li>" ).text( msg )  );
-                var audio = new Audio( "assets/sounds/message.mp3" );
-                audio.play();
-            } );
+socket.on( "chat message", function( msg ) {
+    $( "#messages" ).append( $( "<li>" ).text( msg )  );
+    var audio = new Audio( "assets/sounds/message.mp3" );
+    audio.play();
+} );
 
-            socket.on( "user logout", function( nickname ) {
-                $( "#messages" ).append( $( "<li>" ).css( "font-weight", "700" ).text( nickname + " left us" )  );
-            } );
+socket.on( "user logout", function( nickname ) {
+    $( "#messages" ).append( $( "<li>" ).css( "font-weight", "700" ).text( nickname + " left us" )  );
+} );
+
+function getName() {
+    var userName = null;
+
+    while ( userName === null || userName === "" ) {
+        userName = prompt( "What is your nickname?", "" );
+    }
+
+    return userName.toString();
+}
