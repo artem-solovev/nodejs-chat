dc6ffec0b61ae317467693acf17cd7610773c390
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-15 16:23:18 +0300

    Edited paths for both directories

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'src', included: false},
-      {pattern: 'specs', included: false}
+      {pattern: 'src/', included: false},
+      {pattern: 'specs/', included: false}
     ],
 
 
