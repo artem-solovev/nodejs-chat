044eee79917e39870169ba2144c71cae1b118285
Artem Solovev <artem.solovev.lse@gmail.com>
2016-10-18 16:27:58 +0300

    Added simple test

---
+++ specs/public/clientSpec.js
@@ -0,0 +1,9 @@
+var getName = require( "./getname" );
+
+describe( "Specs for client.js file", function() {
+
+    it( "Checks a string for an empty value", function() {
+        expect( "" ).toBeEqual( "" );
+    } );
+
+} );
